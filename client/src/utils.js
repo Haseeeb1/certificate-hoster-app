0828c04025e1e4ba87960db391d6531a9eb367de
haseeeb1 <haseebhassan701@gmail.com>
2024-09-25 14:26:33 +0500

    url change in utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,4 @@
-export const server_url = "http://localhost:3000";
-// export const server_url = "https://cert-vault-server.vercel.app";
+// export const server_url = "http://localhost:3000";
+export const server_url = "https://cert-vault-server.vercel.app";
 
 export const frontend_url = "http://localhost:5173";

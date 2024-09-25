d509bf2891f2950c23b169cf2cd3b2100a66c1a8
Haseeb Ul Hassan <100109683+Haseeeb1@users.noreply.github.com>
2024-09-25 16:21:25 +0500

    Update utils.js

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,6 @@
 // export const server_url = "http://localhost:3000";
 export const server_url = "https://cert-vault-server.vercel.app";
 
-export const frontend_url = "http://localhost:5173";
+// export const frontend_url = "http://localhost:5173";
+export const frontend_url = "https://cert-vault.vercel.app";
+
